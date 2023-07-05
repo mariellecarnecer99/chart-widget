f66725fbab0edd0831a546418bfb1facbc9f8302
maynion <mcarnecer@startechup.com>
2023-07-05 10:36:24 +0800

    edit data bugfixes

--- src/components/charts/ColumnChart.vue
+++ src/components/charts/ColumnChart.vue
@@ -1,6 +1,62 @@
 <template>
   <ChartData :option="options" />
   <div class="text-center">
+    <v-dialog v-model="dialog" width="500px">
+      <v-card>
+        <v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="6">
+              <v-sheet class="my-2"><h3>Parameter Area</h3></v-sheet>
+            </v-col>
+            <v-col cols="1">
+              <v-sheet class="my-2"
+                ><v-icon @click="dialog = false">mdi-close</v-icon></v-sheet
+              >
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <p class="text-center mb-2">x-Axis</p>
+              <v-select
+                v-model="xAxisData"
+                label="Select x-axis data"
+                :items="['Days', 'Number', 'Category', 'Time']"
+                variant="solo"
+                @update:modelValue="selectedXaxisData"
+              ></v-select>
+            </v-col>
+            <v-col>
+              <p class="text-center mb-2">y-Axis</p>
+              <v-select
+                v-model="yAxisData"
+                label="Select y-axis data"
+                :items="[
+                  'Default',
+                  'Days',
+                  'Precipitation',
+                  'Temperature',
+                  'Category',
+                ]"
+                variant="solo"
+                @update:modelValue="selectedYaxisData"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <!-- <p class="text-center mb-2">Series Data</p>
+          <v-row>
+            <v-col v-for="(item, index) in options.series" :key="index">
+              <v-text-field v-for="i in item.data" variant="outlined">{{
+                i
+              }}</v-text-field>
+            </v-col>
+          </v-row> -->
+        </v-card-text>
+        <!-- <v-card-actions class="d-flex justify-center mb-5">
+          <v-btn color="primary" variant="flat">Save Changes</v-btn>
+        </v-card-actions> -->
+      </v-card>
+    </v-dialog>
+
     <v-dialog v-model="embedDialog" width="500px">
       <v-card>
         <v-card-text>
@@ -42,7 +98,11 @@ export default {
   data: () => {
     return {
       options: null,
+      dialog: false,
       embedDialog: false,
+      xAxisData: [],
+      yAxisData: [],
+      seriesData: [],
       embedChart: "",
     };
   },
@@ -74,6 +134,10 @@ export default {
       myfunc(rest);
     },
 
+    handleDialog() {
+      this.dialog = true;
+    },
+
     handleEmbedCode() {
       this.embedDialog = true;
     },
@@ -86,9 +150,7 @@ export default {
               show: true,
               title: "Edit Data",
               icon: "M33.87,8.32,28,2.42a2.07,2.07,0,0,0-2.92,0L4.27,23.2l-1.9,8.2a2.06,2.06,0,0,0,2,2.5,2.14,2.14,0,0,0,.43,0L13.09,32,33.87,11.24A2.07,2.07,0,0,0,33.87,8.32ZM12.09,30.2,4.32,31.83l1.77-7.62L21.66,8.7l6,6ZM29,13.25l-6-6,3.48-3.46,5.9,6Z",
-              onclick: function () {
-                // do something
-              },
+              onclick: () => this.handleDialog(),
             },
             dataView: { show: true, readOnly: false },
             restore: { show: true },
@@ -121,6 +183,103 @@ export default {
           },
         ],
       };
+    },
+
+    selectedXaxisData(x) {
+      if (x === "Days") {
+        const daysItem = {
+          categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          values: {
+            data: [60, 35, 110, 80, 95, 140, 185],
+          },
+        };
+        this.options.xAxis.data = daysItem.categories;
+        this.options.series = daysItem.values;
+      } else if (x === "Number") {
+        const numberItem = {
+          categories: [1, 2, 3, 4, 5],
+          values: {
+            data: [50, 100, 150, 200, 250],
+          },
+        };
+        this.options.xAxis.data = numberItem.categories;
+        this.options.series = numberItem.values;
+      } else if (x === "Category") {
+        const categoryItem = [
+          "Direct",
+          "Email",
+          "Ad Networks",
+          "Video Ads",
+          "Search Engines",
+        ];
+        this.options.xAxis.data = categoryItem;
+      } else if (x === "Time") {
+        const timeItem = {
+          categories: [10, 40, 70, 100, 130],
+          values: {
+            data: [55, 75, 95, 115, 135],
+          },
+        };
+        this.options.xAxis.data = timeItem.categories;
+        this.options.series = timeItem.values;
+      }
+    },
+
+    selectedYaxisData(y) {
+      if (y === "Default") {
+        const defaultItem = {
+          type: "value",
+          min: 0,
+          max: 200,
+          interval: 50,
+          axisLabel: {
+            formatter: "{value}",
+          },
+        };
+        this.options.yAxis = defaultItem;
+      } else if (y === "Days") {
+        const daysItem = {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        };
+        this.options.yAxis = daysItem;
+      } else if (y === "Precipitation") {
+        const precipitationItem = {
+          type: "value",
+          name: "Precipitation",
+          min: 0,
+          max: 250,
+          interval: 50,
+          axisLabel: {
+            formatter: "{value} ml",
+          },
+        };
+        this.options.yAxis = precipitationItem;
+      } else if (y === "Temperature") {
+        const tempItem = {
+          type: "value",
+          name: "Temperature",
+          min: 0,
+          max: 25,
+          interval: 5,
+          axisLabel: {
+            formatter: "{value} °C",
+          },
+        };
+        this.options.yAxis = tempItem;
+      } else if (y === "Category") {
+        const categoryItem = {
+          type: "category",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines",
+          ],
+        };
+        this.options.yAxis = categoryItem;
+      }
     },
   },
 };
