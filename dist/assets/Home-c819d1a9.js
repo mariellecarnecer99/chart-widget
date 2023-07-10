ca2464301cb5da414bf6261aea69ddd7b2ab1f59
maynion <mcarnecer@startechup.com>
2023-07-10 13:21:34 +0800

    update build files

---
+++ dist/assets/Home-c819d1a9.js
@@ -0,0 +1,1 @@
+import{ay as s,az as t,aK as a,B as c}from"./index-53174ba9.js";const n={class:"my-5 text-center"},i={__name:"Home",props:{title:String,desc:String},setup(e){return(r,l)=>(c(),s("div",n,[t("h2",null,a(e.title),1),t("p",null,a(e.desc),1)]))}};export{i as default};
